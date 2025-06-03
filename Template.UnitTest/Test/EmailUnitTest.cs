1bb8daf8974bd0f163620dc2298d5e673575a0a6
bla-bla-bla-style <5155133+bla-bla-bla-style@users.noreply.github.com>
2025-06-03 22:08:47 +0700

    Add unit test send email body-type html

--- Template.UnitTest/Test/EmailUnitTest.cs
+++ Template.UnitTest/Test/EmailUnitTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Graph.Models;
 using Template.Domain.DTO;
 using Template.Helper.Email;
 using Template.Infrastructure;
@@ -23,7 +24,7 @@ namespace Template.UnitTest.Test
         }
 
         [Fact]
-        public async Task SendEmailSmtpTest()
+        public async Task SendEmailSmtpTextBodyTypeTest()
         {
             bool isSendSuccess = false;
             try
@@ -32,7 +33,7 @@ namespace Template.UnitTest.Test
                 {
                     Subject = "Unit Test Subject",
                     Body = "Unit Test Body",
-                    BodyType = BodyType.TEXT,
+                    BodyType = Domain.DTO.BodyType.TEXT,
                     EmailType = EmailType.SMTP
                 };
 
@@ -80,7 +81,7 @@ namespace Template.UnitTest.Test
         }
 
         [Fact]
-        public async Task SendEmailGraphApiTest()
+        public async Task SendEmailGraphApiTextBodyTypeTest()
         {
             bool isSendSuccess = false;
             try
@@ -89,7 +90,7 @@ namespace Template.UnitTest.Test
                 {
                     Subject = "Unit Test Subject",
                     Body = "Unit Test Body",
-                    BodyType = BodyType.TEXT,
+                    BodyType = Domain.DTO.BodyType.TEXT,
                     EmailType = EmailType.GRAPH_API
                 };
 
@@ -123,6 +124,141 @@ namespace Template.UnitTest.Test
 
                 email.SendTo = sendToList;
                 email.CcTo = ccToList;
+
+                await _email.SendEmailAsync(email);
+
+                isSendSuccess = true;
+            }
+            catch
+            {
+
+            }
+
+            Assert.True(isSendSuccess);
+        }
+
+
+        [Fact]
+        public async Task SendEmailSmtpHtmlBodyTypeTest()
+        {
+            bool isSendSuccess = false;
+            try
+            {
+                var email = new EmailDTO()
+                {
+                    Subject = "Unit Test Subject",
+                    BodyType = Domain.DTO.BodyType.HTML,
+                    EmailType = EmailType.SMTP
+                };
+
+                Assert.NotNull(email);
+
+                var sendTo = new SendTo()
+                {
+                    Email = "unitTestSend@email.com"
+                };
+
+                Assert.NotNull(sendTo);
+
+                var ccTo = new CcTo()
+                {
+                    Email = "unitTestCc@email.com"
+                };
+
+                Assert.NotNull(ccTo);
+
+                var sendToList = new List<SendTo>();
+                sendToList.Add(sendTo);
+
+                Assert.NotNull(sendToList);
+                Assert.NotEmpty(sendToList);
+
+                var ccToList = new List<CcTo>();
+                ccToList.Add(ccTo);
+
+                Assert.NotNull(ccToList);
+                Assert.NotEmpty(ccToList);
+
+                email.SendTo = sendToList;
+                email.CcTo = ccToList;
+
+                var baseDirectory = AppContext.BaseDirectory;
+                string emailTest = File.ReadAllText(Path.Combine(baseDirectory, "Email.html"));
+                emailTest = emailTest.Replace("{subject}", email.Subject);
+                emailTest = emailTest.Replace("{sendTo}", email.SendTo != null ? string.Join(",", email.SendTo.Select(s => s.Name)) : "");
+                emailTest = emailTest.Replace("{ccTo}", email.CcTo != null ? string.Join(",", email.CcTo.Select(s => s.Name)) : "");
+                emailTest = emailTest.Replace("{bodyType}", email.BodyType.ToString());
+                emailTest = emailTest.Replace("{emailType}", email.EmailType.ToString());
+                emailTest = emailTest.Replace("{body}", "Unit Test Body");
+
+                email.Body = emailTest;
+
+                await _email.SendEmailAsync(email);
+
+                isSendSuccess = true;
+            }
+            catch
+            {
+
+            }
+
+            Assert.True(isSendSuccess);
+        }
+
+        [Fact]
+        public async Task SendEmailGraphApiHtmlBodyTypeTest()
+        {
+            bool isSendSuccess = false;
+            try
+            {
+                var email = new EmailDTO()
+                {
+                    Subject = "Unit Test Subject",
+                    BodyType = Domain.DTO.BodyType.TEXT,
+                    EmailType = EmailType.GRAPH_API
+                };
+
+                Assert.NotNull(email);
+
+                var sendTo = new SendTo()
+                {
+                    Email = "unitTestSend@email.com"
+                };
+
+                Assert.NotNull(sendTo);
+
+                var ccTo = new CcTo()
+                {
+                    Email = "unitTestCc@email.com"
+                };
+
+                Assert.NotNull(ccTo);
+
+                var sendToList = new List<SendTo>();
+                sendToList.Add(sendTo);
+
+                Assert.NotNull(sendToList);
+                Assert.NotEmpty(sendToList);
+
+                var ccToList = new List<CcTo>();
+                ccToList.Add(ccTo);
+
+                Assert.NotNull(ccToList);
+                Assert.NotEmpty(ccToList);
+
+                email.SendTo = sendToList;
+                email.CcTo = ccToList;
+
+                var baseDirectory = AppContext.BaseDirectory;
+                string emailTest = File.ReadAllText(Path.Combine(baseDirectory, "Email.html"));
+                emailTest = emailTest.Replace("{subject}", email.Subject);
+                emailTest = emailTest.Replace("{sendTo}", email.SendTo != null ? string.Join(",", email.SendTo.Select(s => s.Name)) : "");
+                emailTest = emailTest.Replace("{ccTo}", email.CcTo != null ? string.Join(",", email.CcTo.Select(s => s.Name)) : "");
+                emailTest = emailTest.Replace("{bodyType}", email.BodyType.ToString());
+                emailTest = emailTest.Replace("{emailType}", email.EmailType.ToString());
+                emailTest = emailTest.Replace("{body}", "Unit Test Body");
+
+                email.Body = emailTest;
 
                 await _email.SendEmailAsync(email);
 
